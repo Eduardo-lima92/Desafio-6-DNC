b5400dc52adaadfdb3866ff06d940832f8a4966c
Eduardo-lima92 <dudulima2005@hotmail.com>
2023-12-28 14:02:05 -0300

    Projeto desafio 6 DNC

---
+++ src/module/pedido/pedido.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('../../config/mongo')
+const { Schema } = mongoose;
+
+const pedidoSchema = new Schema(
+    {
+        dataEntrega: String, 
+        status: String,
+       
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const pedidoModel = mongoose.model('pedido', pedidoSchema);
+
+module.exports = pedidoModel;
